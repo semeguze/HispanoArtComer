00c93f7962d8e9d8c82c573523dc249a9a8547a5
Sebastian Mesa <semeguze@hotmail.com>
2018-11-04 20:46:55 -0500

    Adelanto quiz
    Quiz con op1

--- app/src/main/java/co/edu/javeriana/sebastianmesa/hispanoartcomer/Puntos/PuntosDetailsView.java
+++ app/src/main/java/co/edu/javeriana/sebastianmesa/hispanoartcomer/Puntos/PuntosDetailsView.java
@@ -1,15 +1,57 @@
 package co.edu.javeriana.sebastianmesa.hispanoartcomer.Puntos;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import co.edu.javeriana.sebastianmesa.hispanoartcomer.R;
 
 public class PuntosDetailsView extends AppCompatActivity {
 
+    public Button c1;
+    private ProgressDialog progressBar;
+    public final static String Message = "com.kvikesh800gmail.relativlayoutjava.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puntos_details_view);
+
+        c1 = (Button) findViewById(R.id.b1);
+
+
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //To show button click
+                new Handler().postDelayed(new Runnable() {@Override public void run(){}}, 400);
+
+
+                progressBar = new ProgressDialog(v.getContext());//Create new object of progress bar type
+                progressBar.setCancelable(false);//Progress bar cannot be cancelled by pressing any wher on screen
+                progressBar.setMessage("Getting Questions Ready ...");//Title shown in the progress bar
+                progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);//Style of the progress bar
+                progressBar.setProgress(0);//attributes
+                progressBar.setMax(100);//attributes
+                progressBar.show();//show the progress bar
+                //This handler will add a delay of 3 seconds
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Intent start to open the navigation drawer activity
+                        progressBar.cancel();//Progress bar will be cancelled (hide from screen) when this run function will execute after 3.5seconds
+                        Intent intent = new Intent(PuntosDetailsView.this, Questions.class);
+                        intent.putExtra(Message, "c1");//by this statement we are sending the name of the button that invoked the new Questions.java activity "Message" is defined by the name of the package + MESSAGE
+                        startActivity(intent);
+                    }
+                }, 2000);
+            }
+        });
+
     }
 }
